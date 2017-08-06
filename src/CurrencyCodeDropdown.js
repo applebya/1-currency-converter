664764cf9780c20fd8e0eaeb710703d6c7193aad
Andrew Appleby <andrewappleby22@gmail.com>
2017-08-05 17:21:44 -0700

    Rename to CurrencyCodeDropdown

---
+++ src/CurrencyCodeDropdown.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
+
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    let { selectedCode } = this.props;
+
+    return (
+      <ButtonDropdown isOpen={this.state.isOpen} toggle={this.toggle}>
+        <DropdownToggle caret>
+          {selectedCode || "Currency"}
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>Select a code</DropdownItem>
+          {this.props.currencyCodes.filter(code => code !== selectedCode).map(code => {
+            return (
+              <DropdownItem key={code}>
+                {code}
+              </DropdownItem>
+            );
+          })}
+        </DropdownMenu>
+      </ButtonDropdown>
+    );
+  }
+}
